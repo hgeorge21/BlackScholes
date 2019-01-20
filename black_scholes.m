2c6eee289baca5d27564d9a3da1c71babdc7ccdf
G Huang <yongzhen.huang@mail.utoronto.ca>
2019-01-20 13:14:01 -0500

    initial commit

---
+++ black_scholes.m
@@ -0,0 +1,5 @@
+% CSC446 Course Project
+% Yongzhen Huang
+% Numerical methods for solving the Black-Scholes equation for different
+% types of options (e.g. European, American, Asian)
+% Implements different algorithms for comparison
